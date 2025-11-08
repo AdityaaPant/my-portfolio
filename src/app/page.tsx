41d614368e6408dbf99cf85ea1b863745d8a1188
Aditya Pant <pantanupam05@gmail.com>
2025-11-08 11:26:59 +0530

    fix: portfolio updated avenue ticketing added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,92 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
+import Link from "next/link";
 
 export default function Home() {
-	return (
-		<div>
-			<header>
-				<Header />
-			</header>
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
 
-			<div className="min-h-screen scroll-smooth bg-[#0f0f0f] text-white px-6 py-10 md:px-20 font-sans">
-				<div className="flex flex-col gap-12 max-w-3xl mx-auto text-[17px] leading-relaxed">
-					{/* Hero Section */}
-					<section id="home" className="flex flex-col items-start">
-						<h2 className="text-3xl sm:text-4xl font-bold mb-2">
-							👋 Hey, I&apos;m{" "}
-							<span className="text-[#facc15]">Aditya Pant</span>
-						</h2>
+      <div className="min-h-screen scroll-smooth bg-[#0f0f0f] text-white px-6 py-10 md:px-20 font-sans">
+        <div className="flex flex-col gap-12 max-w-3xl mx-auto text-[17px] leading-relaxed">
+          {/* Hero Section */}
+          <section id="home" className="flex flex-col items-start">
+            <h2 className="text-3xl sm:text-4xl font-bold mb-2">
+              👋 Hey, I&apos;m{" "}
+              <span className="text-[#facc15]">Aditya Pant</span>
+            </h2>
 
-						<p className="text-gray-500 text-sm mb-3">
-							Full Stack Developer focused on clean design, solid performance,
-							and user-first thinking.
-						</p>
+            <p className="text-gray-500 text-sm mb-3">
+              Full Stack Developer focused on clean design, solid performance,
+              and user-first thinking.
+            </p>
 
-						<p className="text-gray-400 border-b border-gray-800 pb-6">
-							I&apos;m a software developer from{" "}
-							<span className="">Uttarakhand, India</span> who loves turning
-							ideas into code.{" "}
-							<span className="font-medium">
-								Evolving with intention, not perfection
-							</span>
-							, I&apos;m all about learning, building, and getting better every
-							day.
-						</p>
-					</section>
+            <p className="text-gray-400 border-b border-gray-800 pb-6">
+              I&apos;m a software developer from{" "}
+              <span className="">Uttarakhand, India</span> who loves turning
+              ideas into code.{" "}
+              <span className="font-medium">
+                Evolving with intention, not perfection
+              </span>
+              , I&apos;m all about learning, building, and getting better every
+              day.
+            </p>
+          </section>
 
-					{/* Work Section */}
-					<section id="projects">
-						<h3 className="text-xl font-semibold text-gray-200 mb-2">
-							What I Do 🚀
-						</h3>
-						<p className="text-gray-400 border-b border-gray-800 pb-6">
-							I&apos;m currently working as a{" "}
-							<span className="font-medium">Full Stack Developer at</span>{" "}
-							<span className="inline-block bg-[#00b4d8]/10 text-[#00b4d8] font-medium px-3 py-[1px] m-1 rounded-md text-sm border border-[#00b4d8]">
-								The Asian Kid
-							</span>
-							, where we&apos;re building a coaching platform to connect
-							teachers and students. The goal is to create a friendly and
-							efficient environment that makes both teaching and learning
-							smooth, accessible, and engaging. From backend logic to frontend
-							UI, I&apos;m helping craft a complete digital experience.
-						</p>
-					</section>
+          {/* Work Section */}
+          <section id="projects">
+            <h3 className="text-xl font-semibold text-gray-200 mb-2">
+              What I Do 🚀
+            </h3>
+            <p className="text-gray-400 border-b border-gray-800 pb-6">
+              I&apos;m currently working as a{" "}
+              <span className="font-medium">Full Stack Intern at</span>{" "}
+              <Link
+                target="_blank"
+                className="inline-block bg-[#00b4d8]/10 text-[#00b4d8] font-medium px-3 py-[1px] m-1 rounded-md text-sm border border-[#00b4d8]"
+                href="https://avenueticket.com"
+              >
+                Avenue Ticketing
+              </Link>
+              is a streamlined platform for buying and managing event tickets. It provides dedicated panels for both event organizers and event attendees, making the entire process simple and efficient. AvenueTicket serves as a one-stop solution for all event-related needs, from ticket sales to event management.
+            </p>
+          </section>
 
-					{/* About Me Section */}
-					<section id="about">
-						<h3 className="text-xl font-semibold text-gray-200 mb-2">
-							About Me 🧑‍💻
-						</h3>
-						<p className="text-gray-400">
-							I&apos;m{" "}
-							<span className="text-[#facc15] font-medium">Aditya Pant</span>, a
-							B.Tech CSE student at{" "}
-							<span className="text-[#94d2bd] font-medium">CGC Landran</span>{" "}
-							with a strong passion for building on the web. I specialize in
-							full stack development using tools like TypeScript, JavaScript,
-							React.js, Next.js, Tailwind CSS, Node.js, PostgreSQL, and MongoDB.
-						</p>
-						<p className="text-gray-400 mt-4">
-							Outside of tech, I&apos;m an{" "}
-							<span className="text-[#f59e0b] font-medium">ex-NCC cadet</span>{" "}
-							with multiple camps attended, and an{" "}
-							<span className="text-[#f472b6] font-medium">
-								active sportsperson
-							</span>{" "}
-							who has represented my college at national, state, and regional
-							levels. I also love traveling to new places and I&apos;m currently
-							working on freelance projects to sharpen my skills.
-						</p>
-					</section>
-				</div>
-			</div>
+          {/* About Me Section */}
+          <section id="about">
+            <h3 className="text-xl font-semibold text-gray-200 mb-2">
+              About Me 🧑‍💻
+            </h3>
+            <p className="text-gray-400">
+              I&apos;m{" "}
+              <span className="text-[#facc15] font-medium">Aditya Pant</span>, a
+              B.Tech CSE student at{" "}
+              <span className="text-[#94d2bd] font-medium">CGC Landran</span>{" "}
+              with a strong passion for building on the web. I specialize in
+              full stack development using tools like TypeScript, JavaScript,
+              React.js, Next.js, Tailwind CSS, Node.js, PostgreSQL, and MongoDB.
+            </p>
+            <p className="text-gray-400 mt-4">
+              Outside of tech, I&apos;m an{" "}
+              <span className="text-[#f59e0b] font-medium">ex-NCC cadet</span>{" "}
+              with multiple camps attended, and an{" "}
+              <span className="text-[#f472b6] font-medium">
+                active sportsperson
+              </span>{" "}
+              who has represented my college at national, state, and regional
+              levels. I also love traveling to new places and I&apos;m currently
+              working on freelance projects to sharpen my skills.
+            </p>
+          </section>
+        </div>
+      </div>
 
-			<footer>
-				<Footer />
-			</footer>
-		</div>
-	);
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,61 +4,70 @@ import Footer from "@/components/ui/footer";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function project() {
-	return (
-		<>
-			<div>
-				<header>
-					<Header />
-				</header>
+  return (
+    <>
+      <div>
+        <header>
+          <Header />
+        </header>
 
-				<div className="min-h-screen scroll-smooth bg-[#0f0f0f] text-white px-6 py-10 md:px-20 font-sans">
-					<section id="projects" className="max-w-3xl scroll-mt-20 mx-auto">
-						<h3 className="text-xl font-semibold text-gray-200 mb-4">
-							🚀 Projects
-						</h3>
-						<div className="flex flex-col gap-6">
-							<ProjectCard
-								title="Coaching Platform"
-								subtitle="The Asian Kid (under Development)"
-								description="A full-featured coaching website that connects students and teachers, creating a smooth, engaging learning environment. It has multiple user interfaces, a guest website , student dashboard , teacher dashboard and admin panel."
-								tech={["Next.js", "Tailwind", "postgreSQL", "Node.js"]}
-								url="https://asian-kid-demo-1l51.vercel.app"
-							/>
-							<ProjectCard
-								title="MyCCET"
-								subtitle="College Management system"
-								description="MyCCET was a college management system made for college staff for maintaining students digitally. Myccet has record of all enrolled students , their results , semesters and batches. They  can create Provisional Certificate. "
-								tech={["Next.js", "Tailwind", "postgreSQL", "Node.js"]}
-							/>
-							<ProjectCard
-								title="Project Jalshakti"
-								subtitle="Water Wiki"
-								description="Project jalshakti was blogging website made for SIH. It was a website made to share knowledge related to water. It was just like wikipedia but for water related knowledge."
-								tech={["EJS", "MongoDB", "Javascript", "Node.js"]}
-								url="https://jalshakti.infyfix.com"
-							/>
-							<ProjectCard
-								title="Time Tracker"
-								subtitle="Time app"
-								description="Track the time and note down the tasks done during that time."
-								tech={["React", "TypeScript", "Tailwind", "nodejs", "mongDB"]}
-								url="https://timetrackerr.vercel.app"
-							/>
-							<ProjectCard
-								title="Raj Bihari"
-								subtitle="Clothing Business"
-								description="A clothing business based in Delhi, India sells premium ethinic women clothing"
-								tech={["Liquid", "HTML", "CSS", "MySQL"]}
-								url="https://rajbihari.in"
-							/>
-						</div>
-					</section>
-				</div>
+        <div className="min-h-screen scroll-smooth bg-[#0f0f0f] text-white px-6 py-10 md:px-20 font-sans">
+          <section id="projects" className="max-w-3xl scroll-mt-20 mx-auto">
+            <h3 className="text-xl font-semibold text-gray-200 mb-4">
+              🚀 Projects
+            </h3>
+            <div className="flex flex-col gap-6">
+              <ProjectCard
+                title="Avenue Ticketing "
+                subtitle="Ticketing Platform"
+                description="AvenueTicket is a smart platform for seamless event ticketing.
+It supports both organizers and attendees with dedicated panels.
+It brings everything related to events into one simple place."
+                tech={["Next.js", "Tailwind", "postgreSQL", "Node.js"]}
+                url="https://avenueticket.com/for-organizers"
+              />
+              <ProjectCard
+                title="The Asian Kid"
+                subtitle="Coaching Platform"
+                description="A full-featured coaching website that connects students and teachers, creating a smooth, engaging learning environment. It has multiple user interfaces, a guest website , student dashboard , teacher dashboard and admin panel."
+                tech={["Next.js", "Tailwind", "postgreSQL", "Node.js"]}
+                url="https://theasiankid.com"
+              />
+              <ProjectCard
+                title="MyCCET"
+                subtitle="College Management system"
+                description="MyCCET was a college management system made for college staff for maintaining students digitally. Myccet has record of all enrolled students , their results , semesters and batches. They  can create Provisional Certificate. "
+                tech={["Next.js", "Tailwind", "postgreSQL", "Node.js"]}
+              />
+              <ProjectCard
+                title="Project Jalshakti"
+                subtitle="Water Wiki"
+                description="Project jalshakti was blogging website made for SIH. It was a website made to share knowledge related to water. It was just like wikipedia but for water related knowledge."
+                tech={["EJS", "MongoDB", "Javascript", "Node.js"]}
+                url="https://jalshakti.infyfix.com"
+              />
+              <ProjectCard
+                title="Time Tracker"
+                subtitle="Time app"
+                description="Track the time and note down the tasks done during that time."
+                tech={["React", "TypeScript", "Tailwind", "nodejs", "mongDB"]}
+                url="https://timetrackerr.vercel.app"
+              />
+              <ProjectCard
+                title="Raj Bihari"
+                subtitle="Clothing Business"
+                description="A clothing business based in Delhi, India sells premium ethinic women clothing"
+                tech={["Liquid", "HTML", "CSS", "MySQL"]}
+                url="https://rajbihari.in"
+              />
+            </div>
+          </section>
+        </div>
 
-				<footer>
-					<Footer />
-				</footer>
-			</div>
-		</>
-	);
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </>
+  );
 }
